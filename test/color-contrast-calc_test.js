4d64ede07631af8a4f4cdd639c35ed51a32085c9
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-05-07 22:49:29 +0900

    group tests for static methods under "static methods"

--- test/color-contrast-calc_test.js
+++ test/color-contrast-calc_test.js
@@ -13,46 +13,48 @@ describe("ColorContrastCalc", () => {
     expect(ColorContrastCalc).to.be.an("function");
   });
 
-  describe("contrastRatio", () => {
-    it("expects to return MAX_CONTRAST when white and black are passed", function() {
-      expect(ColorContrastCalc.contrastRatio(RGB_BLACK, RGB_WHITE)).to.equal(MAX_CONTRAST);
+  describe("static methods", function() {
+    describe("contrastRatio", () => {
+      it("expects to return MAX_CONTRAST when white and black are passed", function() {
+        expect(ColorContrastCalc.contrastRatio(RGB_BLACK, RGB_WHITE)).to.equal(MAX_CONTRAST);
+      });
+
+      it("expects to return MIN_CONTRAST when white and white are passed", function() {
+        expect(ColorContrastCalc.contrastRatio(RGB_WHITE, RGB_WHITE)).to.equal(MIN_CONTRAST);
+
+      });
+
+      it("expects to return 4.23 when white and [127, 127, 32] are passed", function() {
+        expect(ColorContrastCalc.contrastRatio(RGB_WHITE, [127, 127, 32])).to.be.closeTo(4.23, 0.01);
+      });
+
+      it("expects to return 4.23 when #ffffff and #7f7f20 are passed", function() {
+        expect(ColorContrastCalc.contrastRatio("#ffffff", "#7f7f20")).to.be.closeTo(4.23, 0.01);
+      });
     });
 
-    it("expects to return MIN_CONTRAST when white and white are passed", function() {
-      expect(ColorContrastCalc.contrastRatio(RGB_WHITE, RGB_WHITE)).to.equal(MIN_CONTRAST);
+    describe("hexNotationToDecimal", function() {
+      it("expects to return [255, 255, 255] when #ffffff is passed", function() {
+        expect(ColorContrastCalc.hexNotationToDecimal("#ffffff")).to.deep.equal(RGB_WHITE);
+      });
 
+      it("expects to return [0, 0, 0] when #000000 is passed", function() {
+        expect(ColorContrastCalc.hexNotationToDecimal("#000000")).to.deep.equal(RGB_BLACK);
+      });
+
+      it("expects to return [255, 255, 0] when #ffff00 is passed", function() {
+        expect(ColorContrastCalc.hexNotationToDecimal("#ffff00")).to.deep.equal([255, 255, 0]);
+      });
+
+      it("expects to return [255, 255, 0] when #FFFF00 is passed", function() {
+        expect(ColorContrastCalc.hexNotationToDecimal("#FFFF00")).to.deep.equal([255, 255, 0]);
+      });
     });
 
-    it("expects to return 4.23 when white and [127, 127, 32] are passed", function() {
-      expect(ColorContrastCalc.contrastRatio(RGB_WHITE, [127, 127, 32])).to.be.closeTo(4.23, 0.01);
-    });
-
-    it("expects to return 4.23 when #ffffff and #7f7f20 are passed", function() {
-      expect(ColorContrastCalc.contrastRatio("#ffffff", "#7f7f20")).to.be.closeTo(4.23, 0.01);
-    });
-  });
-
-  describe("hexNotationToDecimal", function() {
-    it("expects to return [255, 255, 255] when #ffffff is passed", function() {
-      expect(ColorContrastCalc.hexNotationToDecimal("#ffffff")).to.deep.equal(RGB_WHITE);
-    });
-
-    it("expects to return [0, 0, 0] when #000000 is passed", function() {
-      expect(ColorContrastCalc.hexNotationToDecimal("#000000")).to.deep.equal(RGB_BLACK);
-    });
-
-    it("expects to return [255, 255, 0] when #ffff00 is passed", function() {
-      expect(ColorContrastCalc.hexNotationToDecimal("#ffff00")).to.deep.equal([255, 255, 0]);
-    });
-
-    it("expects to return [255, 255, 0] when #FFFF00 is passed", function() {
-      expect(ColorContrastCalc.hexNotationToDecimal("#FFFF00")).to.deep.equal([255, 255, 0]);
-    });
-  });
-
-  describe("decimalToHexNotation", function() {
-    it("expects to return #fff00 when [255, 255, 0] is passed", function() {
-      expect(ColorContrastCalc.decimalToHexNotation([255, 255, 0])).to.equal("#ffff00");
+    describe("decimalToHexNotation", function() {
+      it("expects to return #fff00 when [255, 255, 0] is passed", function() {
+        expect(ColorContrastCalc.decimalToHexNotation([255, 255, 0])).to.equal("#ffff00");
+      });
     });
   });
 });
