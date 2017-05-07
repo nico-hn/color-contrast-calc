993478d90ca932fcb97301b05f0e6c7420861317
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-05-07 16:52:44 +0900

    the first commit

---
+++ test/color-contrast-calc_test.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const expect = require("chai").expect;
+const ColorContrastCalc = require("../lib/color-contrast-calc").ColorContrastCalc;
+
+describe("ColorContrastCalc", () => {
+  const MIN_CONTRAST = 1.0;
+  const MAX_CONTRAST = 21.0;
+  const RGB_BLACK = [0, 0, 0];
+  const RGB_WHITE = [255, 255, 255];
+
+  it("canary test", function() {
+    expect(ColorContrastCalc).to.be.an("function");
+  });
+
+  describe("contrastRatio", () => {
+    it("expects to return MAX_CONTRAST when white and black are passed", function() {
+      const calc = new ColorContrastCalc();
+
+      expect(calc.contrastRatio(RGB_BLACK, RGB_WHITE)).to.equal(MAX_CONTRAST);
+    });
+
+    it("expects to return MIN_CONTRAST when white and white are passed", function() {
+      const calc = new ColorContrastCalc();
+
+      expect(calc.contrastRatio(RGB_WHITE, RGB_WHITE)).to.equal(MIN_CONTRAST);
+
+    });
+
+    it("expects to return 4.23 when white and [127, 127, 32] are passed", function() {
+      const calc = new ColorContrastCalc();
+
+      expect(calc.contrastRatio(RGB_WHITE, [127, 127, 32])).to.be.closeTo(4.23, 0.01);
+    });
+  });
+});
+
