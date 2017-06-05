14c9e154c1744e9fcc59521ede0d6809f0d5853f
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-06-05 10:03:17 +0900

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const babelify = require("babelify");
+
+gulp.task("es5", () =>
+          browserify("./index.js")
+          .transform(babelify)
+          .bundle()
+          .pipe(source("color-contrast-calc-es5.js"))
+          .pipe(gulp.dest("./dist/")));
+
+gulp.task("es6", () =>
+          browserify("./index.js")
+          .bundle()
+          .pipe(source("color-contrast-calc-es6.js"))
+          .pipe(gulp.dest("./dist/")));
+
+
+gulp.task("default", ["es5", "es6"]);
