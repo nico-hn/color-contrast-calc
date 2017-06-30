e466aef1dd12c5a2e797ab1197532073f236d596
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-06-30 14:21:05 +0900

    group the existing tests for ColorContrastCalc.sort() into a context()

--- test/color-contrast-calc_test.js
+++ test/color-contrast-calc_test.js
@@ -216,20 +216,22 @@ describe("ColorContrastCalc", () => {
       const yellow = new ColorContrastCalc("#ffff00");
       const orange = new ColorContrastCalc("#ffa500");
 
-      it("expects to return [black, orange, yellow] when [black, yellow, orange] is passed", function() {
-        expect(ColorContrastCalc.sort([black, yellow, orange])).to.deep.equal([black, orange, yellow]);
-      });
+      context("when colorOrder is 'rgb'", function() {
+        it("expects to return [black, orange, yellow] when [black, yellow, orange] is passed", function() {
+          expect(ColorContrastCalc.sort([black, yellow, orange])).to.deep.equal([black, orange, yellow]);
+        });
 
-      it("expects to return [black, orange, yellow] when [yellow, black, orange] is passed", function() {
-        expect(ColorContrastCalc.sort([yellow, black, orange])).to.deep.equal([black, orange, yellow]);
-      });
+        it("expects to return [black, orange, yellow] when [yellow, black, orange] is passed", function() {
+          expect(ColorContrastCalc.sort([yellow, black, orange])).to.deep.equal([black, orange, yellow]);
+        });
 
-      it("expects to return [black, gray, orange, yellow] when [yellow, black, orange, gray] is passed", function() {
-        expect(ColorContrastCalc.sort([yellow, black, orange, gray])).to.deep.equal([black, gray, orange, yellow]);
-      });
+        it("expects to return [black, gray, orange, yellow] when [yellow, black, orange, gray] is passed", function() {
+          expect(ColorContrastCalc.sort([yellow, black, orange, gray])).to.deep.equal([black, gray, orange, yellow]);
+        });
 
-      it("expects to return [black, blue, orange, yellow] when [yellow, black, orange, blue] is passed", function() {
-        expect(ColorContrastCalc.sort([yellow, black, orange, blue])).to.deep.equal([black, blue, orange, yellow]);
+        it("expects to return [black, blue, orange, yellow] when [yellow, black, orange, blue] is passed", function() {
+          expect(ColorContrastCalc.sort([yellow, black, orange, blue])).to.deep.equal([black, blue, orange, yellow]);
+        });
       });
     });
 
