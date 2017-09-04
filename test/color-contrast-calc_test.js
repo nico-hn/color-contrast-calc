401be8a0ea51403733fbfd09408cfe1c065a55a7
HASHIMOTO, Naoki <hashimoto.naok@gmail.com>
2017-09-04 11:48:57 +0900

    update test cases: move instance method ColorContrastCalc.prototype.newHslColor() to Color

--- test/color-contrast-calc_test.js
+++ test/color-contrast-calc_test.js
@@ -650,11 +650,11 @@ describe("ColorContrastCalc", () => {
 
     describe("newHslColor", function() {
       it("expects to return an instance with .hexCode '#ffff00' when [60, 100, 50]  is passed", function() {
-        expect(ColorContrastCalc.newHslColor([60, 100, 50]).hexCode).to.equal("#ffff00");
+        expect(Color.newHslColor([60, 100, 50]).hexCode).to.equal("#ffff00");
       });
 
       it("expects to return an instance with .hexCode '#ff8000' when [30, 100, 50]  is passed", function() {
-        expect(ColorContrastCalc.newHslColor([30, 100, 50]).hexCode).to.equal("#ff8000");
+        expect(Color.newHslColor([30, 100, 50]).hexCode).to.equal("#ff8000");
       });
     });
 
