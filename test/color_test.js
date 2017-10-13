64a6520186d8170b88abd3ba5f66577585494e86
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-13 17:01:22 +0900

    add colot_test.js

---
+++ test/color_test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const expect = require("chai").expect;
+const ColorContrastCalc = require("../lib/color-contrast-calc").ColorContrastCalc;
+const Color = require("../lib/color-contrast-calc").Color;
+
+describe("Color", () => {
+});
+
