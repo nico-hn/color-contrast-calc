310a9cfcbca108dbb90cd7c6e456fcbfc1f4c349
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-13 17:03:41 +0900

    move test for Color.newHslColor() to color_test.js

--- test/color-contrast-calc_test.js
+++ test/color-contrast-calc_test.js
@@ -648,16 +648,6 @@ describe("ColorContrastCalc", () => {
       });
     });
 
-    describe("newHslColor", function() {
-      it("expects to return an instance with .hexCode '#ffff00' when [60, 100, 50]  is passed", function() {
-        expect(Color.newHslColor([60, 100, 50]).hexCode).to.equal("#ffff00");
-      });
-
-      it("expects to return an instance with .hexCode '#ff8000' when [30, 100, 50]  is passed", function() {
-        expect(Color.newHslColor([30, 100, 50]).hexCode).to.equal("#ff8000");
-      });
-    });
-
     describe("binarySearchRange", function() {
       it("expects to return a smaller value for each iteration", function() {
         let ds = [];
--- test/color_test.js
+++ test/color_test.js
@@ -5,5 +5,16 @@ const ColorContrastCalc = require("../lib/color-contrast-calc").ColorContrastCal
 const Color = require("../lib/color-contrast-calc").Color;
 
 describe("Color", () => {
+  describe("static methods", function() {
+    describe("newHslColor", function() {
+      it("expects to return an instance with .hexCode '#ffff00' when [60, 100, 50]  is passed", function() {
+        expect(Color.newHslColor([60, 100, 50]).hexCode).to.equal("#ffff00");
+      });
+
+      it("expects to return an instance with .hexCode '#ff8000' when [30, 100, 50]  is passed", function() {
+        expect(Color.newHslColor([30, 100, 50]).hexCode).to.equal("#ff8000");
+      });
+    });
+  });
 });
 
