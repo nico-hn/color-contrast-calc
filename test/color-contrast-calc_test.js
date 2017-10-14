8b10fd7262fbbdb03570e9c29b22ae977d52ce35
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-14 15:36:16 +0900

    remove ColorContrastCalc.contrastRatio()

--- test/color-contrast-calc_test.js
+++ test/color-contrast-calc_test.js
@@ -5,8 +5,6 @@ const ColorContrastCalc = require("../lib/color-contrast-calc").ColorContrastCal
 const Color = require("../lib/color-contrast-calc").Color;
 
 describe("ColorContrastCalc", () => {
-  const MIN_CONTRAST = 1.0;
-  const MAX_CONTRAST = 21.0;
   const RGB_BLACK = [0, 0, 0];
   const RGB_WHITE = [255, 255, 255];
 
@@ -15,25 +13,6 @@ describe("ColorContrastCalc", () => {
   });
 
   describe("static methods", function() {
-    describe("contrastRatio", () => {
-      it("expects to return MAX_CONTRAST when white and black are passed", function() {
-        expect(ColorContrastCalc.contrastRatio(RGB_BLACK, RGB_WHITE)).to.equal(MAX_CONTRAST);
-      });
-
-      it("expects to return MIN_CONTRAST when white and white are passed", function() {
-        expect(ColorContrastCalc.contrastRatio(RGB_WHITE, RGB_WHITE)).to.equal(MIN_CONTRAST);
-
-      });
-
-      it("expects to return 4.23 when white and [127, 127, 32] are passed", function() {
-        expect(ColorContrastCalc.contrastRatio(RGB_WHITE, [127, 127, 32])).to.be.closeTo(4.23, 0.01);
-      });
-
-      it("expects to return 4.23 when #ffffff and #7f7f20 are passed", function() {
-        expect(ColorContrastCalc.contrastRatio("#ffffff", "#7f7f20")).to.be.closeTo(4.23, 0.01);
-      });
-    });
-
     describe("NAMED_COLORS", function() {
       it("expects to contain predefined instances of ColorContrastCalc", function() {
         expect(ColorContrastCalc.NAMED_COLORS[0]).to.be.instanceof(Color);
