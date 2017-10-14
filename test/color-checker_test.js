30862ebd51a805f61b915189bd6dc060fb03546b
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-14 15:20:48 +0900

    copy ColorContrastCalc.ContrastRatio() and related methods to ColorChecker

--- test/color-checker_test.js
+++ test/color-checker_test.js
@@ -5,4 +5,29 @@ const Checker = require("../lib/color-checker").ColorChecker;
 
 
 describe("ColorChecker", () => {
+  const MIN_CONTRAST = 1.0;
+  const MAX_CONTRAST = 21.0;
+  const RGB_BLACK = [0, 0, 0];
+  const RGB_WHITE = [255, 255, 255];
+
+  describe("static methods", function() {
+    describe("contrastRatio", () => {
+      it("expects to return MAX_CONTRAST when white and black are passed", function() {
+        expect(Checker.contrastRatio(RGB_BLACK, RGB_WHITE)).to.equal(MAX_CONTRAST);
+      });
+
+      it("expects to return MIN_CONTRAST when white and white are passed", function() {
+        expect(Checker.contrastRatio(RGB_WHITE, RGB_WHITE)).to.equal(MIN_CONTRAST);
+
+      });
+
+      it("expects to return 4.23 when white and [127, 127, 32] are passed", function() {
+        expect(Checker.contrastRatio(RGB_WHITE, [127, 127, 32])).to.be.closeTo(4.23, 0.01);
+      });
+
+      it("expects to return 4.23 when #ffffff and #7f7f20 are passed", function() {
+        expect(Checker.contrastRatio("#ffffff", "#7f7f20")).to.be.closeTo(4.23, 0.01);
+      });
+    });
+  });
 });
