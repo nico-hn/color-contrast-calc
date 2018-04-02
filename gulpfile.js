0b323ef99d6baaea455348e443b0cc9f3190c2b8
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2018-04-02 18:27:10 +0900

    cosmetic change to suppress warnings of eslint

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ const browserify = require("browserify");
 const source = require("vinyl-source-stream");
 const babelify = require("babelify");
 
-gulp.task("es5", () =>
-          browserify("./index.js", { standalone: "ColorContrastCalc" })
+gulp.task("es5", () => browserify("./index.js",
+                                  { standalone: "ColorContrastCalc" })
           .transform(babelify)
           .bundle()
           .pipe(source("color-contrast-calc-es5.js"))
           .pipe(gulp.dest("./dist/")));
 
-gulp.task("es6", () =>
-          browserify("./index.js", { standalone: "ColorContrastCalc" })
+gulp.task("es6", () => browserify("./index.js",
+                                  { standalone: "ColorContrastCalc" })
           .bundle()
           .pipe(source("color-contrast-calc-es6.js"))
           .pipe(gulp.dest("./dist/")));
